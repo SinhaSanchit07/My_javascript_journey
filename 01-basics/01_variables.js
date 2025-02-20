b2fac9410b1365a291b0c745fd1d45324f7b350b
Sanchit Sinha <168222060+SinhaSanchit07@users.noreply.github.com>
2025-02-20 17:52:12 +0000

    variables

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=123456
+let accountEmail="jecrc@edu.in"
+var accountPassword="qwerty"
+accountCity="jaipur"
+
+let accountState;
+
+// accountId=3456 // not allowed
+
+// console.log(accountId); // cannot change the value of const 
+
+accountEmail="sanchit@jecrc.edu.in"
+accountPassword="1234"
+accountCity="udaipur"
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState]);
