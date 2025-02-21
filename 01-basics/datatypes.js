b206c697ead32566cf28815af41744300863de6c
Sanchit Sinha <168222060+SinhaSanchit07@users.noreply.github.com>
2025-02-21 03:35:33 +0000

    datatypes

---
+++ 01-basics/datatypes.js
@@ -0,0 +1,32 @@
+" use strict "// treat all JS code as a newer version
+// alert( 3+ 3); // this is node js not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log(" good morning");
+
+let number=10;
+let string = "hello world "
+let boolean = true 
+let undefined;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+console.log(typeof null);
+console.log(typeof number);
+console.log(typeof undefined);
+console.log( typeof string);
+console.log(typeof boolean);
+
+
+
+
+
