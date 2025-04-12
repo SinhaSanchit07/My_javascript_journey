b4eb4ea1b6911ab345043d265a862c6d98204338
Sanchit Sinha <168222060+SinhaSanchit07@users.noreply.github.com>
2025-04-12 12:22:36 +0000

    comaprison

---
+++ 01-basics/comparison.js
@@ -0,0 +1,20 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null >0)
+console.log(null ==0)
+console.log(null >=0)
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//=== this is a triple equal operator that checks the value and the type strictly 
+
+console.log("2"===2);
