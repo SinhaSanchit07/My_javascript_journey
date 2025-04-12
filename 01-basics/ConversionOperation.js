b66cd844bce080c0bb268f09674620218c8148af
Sanchit Sinha <168222060+SinhaSanchit07@users.noreply.github.com>
2025-04-12 00:32:54 +0000

    datatype

---
+++ 01-basics/ConversionOperation.js
@@ -0,0 +1,41 @@
+let Score="33";
+
+console.log(Score);
+console.log(typeof(Score));
+let ValueInnumber = Number(Score);
+console.log(ValueInnumber);
+console.log(typeof(ValueInnumber));
+
+let ValueInboolean = Boolean(Score);
+console.log(ValueInboolean);
+console.log(typeof(ValueInboolean));
+
+let value = null;
+console.log(value);
+console.log(typeof(value));
+let Say = Number(value);
+console.log(Say);
+console.log(typeof(Say));
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+
+
+/*
+"33"=>33
+33=>true
+""=>false  "sanchit"=>true
+null=>0 
+"33abc"=>NaN
+1 => true; 0 => false
+
+*/ 
+
+
+/**********************************************operation********************************************************************* */
+
+
