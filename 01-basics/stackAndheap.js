482b173829e0f96b4aa5a96ce66457b7dad332ee
Sanchit Sinha <168222060+SinhaSanchit07@users.noreply.github.com>
2025-04-14 12:04:38 +0000

    stack and heap memory

---
+++ 01-basics/stackAndheap.js
@@ -0,0 +1,22 @@
+let userId1="sinhasanchit07@gmail.com";
+
+let userId2 = userId1;
+userId2= "yourmail@gmail.com";
+
+console.log(userId1);
+
+console.log(userId2);
+
+let userone={
+    email:"mymail@yahoo.com",
+    upi:"user@ybl"
+}
+
+let usertwo = userone;
+
+usertwo.upi="user@ptsbi";
+console.log(userone.upi);
+console.log(usertwo.upi);
+
+
+
