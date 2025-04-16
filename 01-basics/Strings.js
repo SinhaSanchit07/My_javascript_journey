ec497a52793bfa9a6bd3dd277f40fe5123a0e094
Sanchit Sinha <168222060+SinhaSanchit07@users.noreply.github.com>
2025-04-16 18:03:18 +0000

    strings

---
+++ 01-basics/Strings.js
@@ -0,0 +1,46 @@
+const name ="sanchit";
+const repocount=100;
+
+console.log(name , repocount,"value");
+
+console.log(`hello my name is ${name} and my repo count is ${repocount}`);
+
+const gamename= new String("sanchit-ss-com");
+
+console.log(gamename.__proto__);
+console.log(gamename[1]);
+
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(3));
+console.log(gamename.indexOf("i"));
+
+const newString = gamename.substring(0,4);
+console.log(newString);
+
+const anotherString = gamename.slice(0,4);
+console.log(anotherString);
+
+// const str1= "    HELLO  World   ";
+// console.log(str1);
+// console.log(str1.trim());
+
+// const url = "https://hitesh.com/hitesh%20choudhary"
+
+// console.log(url.replace("%20","-"));
+
+// console.log(url.includes('sundar'))
+
+// console.log(gamename.split("-"));
+
+
+
+
+
+
+
+
+
+
+
+
