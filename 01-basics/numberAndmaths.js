11c9b2b0e0d35048723b6a794be57bb48fe81478
Sanchit Sinha <168222060+SinhaSanchit07@users.noreply.github.com>
2025-04-18 12:29:28 +0000

    number and maths

---
+++ 01-basics/numberAndmaths.js
@@ -0,0 +1,47 @@
+const number=100;
+// console.log(number);
+
+const balance= new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(4));
+
+const anothernumber = 112.243;
+// console.log(anothernumber.toPrecision(2));
+
+
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++++++    Maths    +++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.PI);
+console.log((Math.round(22.45)));
+console.log(Math.floor(89.7));
+console.log(Math.ceil(88.2));
+console.log(Math.min(4, 3, 6, 8));
+ console.log(Math.max(4, 3, 6, 8));
+
+
+ console.log(Math.random());
+    console.log(Math.random()*10 +1);
+    console.log((Math.random()*10)+1)
+    console.log(Math.floor((Math.random()*10)+1));
+    
+
+    const min=10;
+    const max= 20;
+
+    console.log(Math.floor(Math.random()*(max-min+1)+ min));
+    
+    
+    
+ 
+
+
+
+
